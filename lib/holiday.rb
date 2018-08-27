5c2c85cce712cb9748a31206944b5f1c635003d0
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-08-27 22:57:05 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -32,7 +32,7 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  holiday_hash[spring]
+  holiday_hash[:spring]
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
