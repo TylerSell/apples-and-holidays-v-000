9d227c219396bf20873375d9a9f72f61fbdcac2b
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-08-27 23:15:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday, items|
-      puts "#{holiday.to_s.split("_").map {|each| each.capitalize}"
+      puts "#{holiday.to_s.split("_").map {|each| each.capitalize}.join(" ")}"
 end
 
 def all_holidays_with_bbq(holiday_hash)
